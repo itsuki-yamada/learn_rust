df7c0fa4af62f0d083805eb13d72d6e29bcb2f30
ItsukiYamada <itsuki.yamada@uzabase.com>
2022-07-03 20:40:00 +0900

    feat: 関数呼び出し

---
+++ rust_jp/lean_functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_functions(5);
+}
+
+fn another_functions(x: i32) {
+    println!("Another functinos");
+    println!("The value of x is {}", x);
+}
