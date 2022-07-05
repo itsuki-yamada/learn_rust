3754aa1ce69cbbf9149100e7486bb48fe0d37179
ItsukiYamada <itsuki.yamada@uzabase.com>
2022-07-05 23:14:59 +0900

    refactor: 関数に切り出し

--- rusthanzuon-at-rust-ca-1-day-youth-boot-camp/src/main.rs
+++ rusthanzuon-at-rust-ca-1-day-youth-boot-camp/src/main.rs
@@ -1,14 +1,19 @@
 fn main() {
-    for num in 0..100 { // 0 <= num < 100
-        let fizzbuzz = if num % 15 == 0 {
-            "FizzBuzz".to_string()
-        } else if num % 3 == 0 {
-            "Fizz".to_string()
-        } else if num % 5 == 0 {
-            "Buzz".to_string()
-        } else {
-            num.to_string()
-        };
+    for num in 0..100 {
+        // 0 <= num < 100
+        let fizzbuzz = fizzbuzz(num);
         println!("{}", fizzbuzz);
+    }
+}
+
+fn fizzbuzz(num: i32) -> String {
+    if num % 15 == 0 {
+        "FizzBuzz".to_string()
+    } else if num % 3 == 0 {
+        "Fizz".to_string()
+    } else if num % 5 == 0 {
+        "Buzz".to_string()
+    } else {
+        num.to_string()
     }
 }
