c350a6263aee5f2d3a7fd4eb49aca059dbefb2c9
ItsukiYamada <itsuki.yamada@uzabase.com>
2022-07-06 23:49:59 +0900

    refactor: cat

--- rusthanzuon-at-rust-ca-1-day-youth-boot-camp/cat/src/main.rs
+++ rusthanzuon-at-rust-ca-1-day-youth-boot-camp/cat/src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::read_to_string;
 
 fn main() {
-    run_cat();
+    match std::env::args().nth(1) {
+        Some(path) => run_cat(path),
+        None => println!("No path is specifed!"),
+    }
 }
 
-fn run_cat() {
-    let path = "./src/main.rs";
+fn run_cat(path: String) {
     match read_to_string(path) {
         Ok(content) => println!("{}", content),
         Err(reason) => println!("{}", reason),
