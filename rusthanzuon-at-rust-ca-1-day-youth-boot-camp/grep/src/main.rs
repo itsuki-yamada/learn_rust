f30768d4f1e589bb64beb2bb7345002079bf9a19
ItsukiYamada <itsuki.yamada@uzabase.com>
2022-07-15 00:27:43 +0900

    feat: grep

--- rusthanzuon-at-rust-ca-1-day-youth-boot-camp/grep/src/main.rs
+++ rusthanzuon-at-rust-ca-1-day-youth-boot-camp/grep/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::read_to_string;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "rsgrep")]
+struct GrepArgs {
+    #[structopt(name = "PATTERN")]
+    pattern: String,
+    #[structopt(name = "FILE")]
+    path: Vec<String>,
+}
+
+fn grep(content: &str, pattern: &str, path: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}: {}", line, &path);
+        }
+    }
+}
+
+fn run(state: GrepArgs) {
+    for path in state.path.iter() {
+        match read_to_string(path) {
+            Ok(content) => grep(&content, &state.pattern, path),
+            Err(reason) => println!("{}", reason),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run(GrepArgs::from_args());
 }
